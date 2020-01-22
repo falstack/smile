9502acddb7a7f6d85c50a055bf480e925d4ec719
falstack <icesilt@outlook.com>
2020-01-22 09:55:29 +0800

    add: pm2 script

---
+++ pm2.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'smile',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      watch: false,
+      instances: 'max',
+      ignore_watch: 'node_modules',
+      env: {
+        RELEASE: 'local',
+        NODE_ENV: 'development'
+      },
+      env_staging: {
+        RELEASE: 'stage',
+        NODE_ENV: 'staging'
+      },
+      env_production: {
+        RELEASE: 'prod',
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+}
