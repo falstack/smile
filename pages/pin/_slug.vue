093b5a4c3c65e1999b0cf55d53d3b38cd6da0838
falstack <icesilt@outlook.com>
2020-02-03 12:48:44 +0800

    add: pin title

--- pages/pin/_slug.vue
+++ pages/pin/_slug.vue
@@ -1,11 +1,40 @@
 <style lang="scss">
 #pin-show {
   padding: 0 $page-padding;
+
+  .title {
+    color: $color-text-1;
+    font-weight: 600;
+    font-synthesis: style;
+    font-size: 24px;
+    line-height: 1.22;
+    margin-top: 20px;
+    margin-bottom: 10px;
+    @extend %breakWord;
+  }
+
+  .state {
+    font-size: 12px;
+    margin-bottom: 10px;
+    color: $color-text-3;
+  }
 }
 </style>
 
 <template>
   <div id="pin-show">
+    <p v-if="title" class="title" v-text="title.text" />
+    <div class="state">
+      <template v-if="bangumi">
+        <span @click="handleBangumiClick" v-text="bangumi.name" />
+        <span class="dot">&nbsp;·&nbsp;</span>
+      </template>
+      <span v-text="$utils.timeAgo(published_at)" />
+      <template v-if="visit_count">
+        <span class="dot">&nbsp;·&nbsp;</span>
+        <span v-text="`${$utils.shortenNumber(visit_count)}阅读`" />
+      </template>
+    </div>
     <JsonContent :slug="slug" :content="content" :reward="reward_status" :vote="vote_hash" />
   </div>
 </template>
@@ -36,13 +65,13 @@ export default {
       intro: '',
       title: null,
       author: null,
+      bangumi: null,
       content: [],
       trial_type: 0,
       comment_type: 0,
       last_top_at: 0,
       recommended_at: 0,
       published_at: '',
-      created_at: '',
       last_edit_at: '',
       reward_status: false,
       like_count: 0,
@@ -51,6 +80,13 @@ export default {
       comment_count: 0,
       reward_count: 0,
       vote_hash: []
+    }
+  },
+  methods: {
+    handleBangumiClick() {
+      this.$bridge.navigateTo({
+        url: `/pages/bangumi/show/index?slug=${this.bangumi.slug}`
+      })
     }
   }
 }
