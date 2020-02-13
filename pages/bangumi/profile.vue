735b2fd884bf3b96571d66277a006688af906ab9
falstack <icesilt@outlook.com>
2020-02-13 09:08:54 +0800

    update: edit bangumi

--- pages/bangumi/profile.vue
+++ pages/bangumi/profile.vue
@@ -1,73 +1,68 @@
 <style lang="scss">
 #profile-bangumi {
-  padding: 15px;
+  padding: $page-padding 0;
 
-  .avatar-field {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
+  .alias {
+    line-height: 44px;
 
-    .avatar {
-      width: 100px;
-      height: 100px;
-      border-radius: 10%;
-      margin-right: 15px;
+    button {
+      margin-right: 5px;
     }
   }
 }
 </style>
 
 <template>
-  <div v-if="bangumi" id="profile-bangumi">
-    <div ref="form" label-position="top" label-width="80px">
-      <div label="名称">
-        <VField v-model="bangumi.name" disabled />
-      </div>
-      <div label="头像">
-        <div class="avatar-field">
-          <img :src="$resize(bangumi.avatar, { width: 100 })" class="avatar">
-          <VUploader
-            :show-file-list="false"
-            :action="imageUploadAction"
-            :limit="uploadImageLimit"
-            :data="uploadHeaders"
-            :accept="imageUploadAccept"
-            :before-upload="handleImageUploadBefore"
-            :on-success="avatarUploadSuccess"
-            :on-error="handleImageUploadError"
-          >
-            <VButton :loading="!!uploadPending" type="success" plain round size="mini">
-              {{ uploadPending ? '图片上传中...' : '点击上传头像' }}
-            </VButton>
-          </VUploader>
-        </div>
-      </div>
-      <div label="别名">
-        {{ bangumi.alias }}
-      </div>
-      <div label="简介">
-        <VField v-model="bangumi.intro" type="textarea" rows="4" resize="vertical" />
-      </div>
-      <div>
-        <VButton type="success" round @click="handleSubmit">
-          提交
+  <VForm v-if="bangumi" id="profile-bangumi" :form="bangumi" :rule="rule" @submit="handleSubmit">
+    <VField v-model="bangumi.name" disabled label="名称" />
+    <VField label="头像">
+      <VUploader
+        v-model="bangumi.avatar"
+        :cookie="false"
+        required
+        :url="imageUploadAction"
+        :accept="imageUploadAccept"
+        :transform-request="imageUploadRequest"
+        :transform-response="imageUploadResponse"
+      />
+    </VField>
+    <VField label="别名">
+      <div class="alias">
+        <VButton v-for="(name, index) in bangumi.alias" :key="name" size="small" plain>
+          <span v-text="name" />
+          <i class="bili-font ic_input_close" @click="removeAlias(index)" />
         </VButton>
       </div>
-    </div>
-  </div>
+      <VField v-model="alias" :close="false">
+        <template #after>
+          <VButton @click="addAlias">
+            添加
+          </VButton>
+        </template>
+      </VField>
+    </VField>
+    <VField
+      v-model="bangumi.intro"
+      label="简介"
+      :min-row="4"
+      :max-row="10"
+      :max-len="500"
+      counter
+    />
+  </VForm>
 </template>
 
 <script>
-import { VButton, VField, VUploader } from '@calibur/sakura'
+import { VButton, VField, VUploader, VForm } from '@calibur/sakura'
 import mustSign from '~/mixins/mustSign'
 import upload from '~/mixins/upload'
 
 export default {
   name: 'ProfileBangumi',
   components: {
-    VButton,
+    VForm,
     VField,
+    VButton,
     VUploader
   },
   mixins: [mustSign, upload],
@@ -86,15 +81,43 @@ export default {
       .catch(error)
   },
   data() {
+    const validateAlias = (rule, value, callback) => {
+      if (!value || !value.length) {
+        callback(new Error('别名不能为空'))
+      }
+      if (value.some(_ => /,/.test(_))) {
+        callback(new Error('别名不能包含英文逗号'))
+      }
+      if (value.join('').length > 100) {
+        callback(new Error('别名最多100个字符'))
+      }
+      callback()
+    }
     return {
       bangumi: null,
+      rule: {
+        alias: {
+          type: 'array',
+          validator: validateAlias
+        }
+      },
+      alias: '',
       loading: false
     }
   },
   methods: {
-    avatarUploadSuccess(res, file) {
-      this.handleImageUploadSuccess(res, file)
-      this.bangumi.avatar = res.data.url
+    removeAlias(index) {
+      this.bangumi.alias.splice(index, 1)
+    },
+    addAlias() {
+      if (!this.alias) {
+        return
+      }
+      if (this.bangumi.alias.includes(this.alias)) {
+        return
+      }
+      this.bangumi.alias.push(this.alias)
+      this.alias = ''
     },
     handleSubmit() {
       if (this.loading) {
