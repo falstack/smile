246f35d4b9294dc9edbf9f52bf116effabeb93bf
falstack <icesilt@outlook.com>
2020-02-13 18:08:08 +0800

    update: bangumi

---
+++ pages/bangumi/rule.vue
@@ -0,0 +1,133 @@
+<style lang="scss">
+#bangumi-rule {
+  .form-tip {
+    font-size: 12px;
+    color: $color-orange;
+  }
+}
+</style>
+
+<template>
+  <VForm v-if="rule" id="bangumi-rule" full :loading="loading" @submit="handleSubmit">
+    <VField :label="`出题数（${rule.question_count}个）`">
+      <p class="form-tip">
+        如果题库数量不足，则能出几题出几题，如果题库数量超过设定值，则随机出指定题目数
+      </p>
+      <VRange v-model="rule.question_count" :min="5" :max="100" />
+    </VField>
+    <VField :label="`正确率（${rule.right_rate}%）`">
+      <p class="form-tip">
+        答题的正确率不低于该值才能通过
+      </p>
+      <VRange v-model="rule.right_rate" :min="50" :max="100" />
+    </VField>
+    <!--
+    <ElFormItem label="答题时长">
+      <p class="form-tip">
+        答题超过该时长之后就自动设为失败
+      </p>
+      <ElSlider
+        v-model="rule.qa_minutes"
+        :min="30"
+        :max="120"
+        :format-tooltip="formatQAMinutes"
+      />
+    </ElFormItem>
+    <ElFormItem label="加入方式">
+      <ElRadioGroup v-model="rule.rule_type">
+        <ElRadio :label="0">
+          需要答题或管理邀请
+        </ElRadio>
+        <ElRadio :label="1">
+          只能管理邀请
+        </ElRadio>
+        <ElRadio :label="2">
+          只能答题加入
+        </ElRadio>
+      </ElRadioGroup>
+      <p class="form-tip">
+        加入方式更改之后不会影响「正在答题」和「已经加入」的人
+      </p>
+    </ElFormItem>
+    <ElFormItem label="考核方式">
+      <ElRadioGroup v-model="rule.result_type">
+        <ElRadio :label="0">
+          答完所有题目之后只告知是否通过
+        </ElRadio>
+        <ElRadio :label="1">
+          每答一道题就得知对错（该模式下正确率强制为100%）
+        </ElRadio>
+      </ElRadioGroup>
+    </ElFormItem>
+    -->
+  </VForm>
+</template>
+
+<script>
+import { VForm, VRange, VField } from '@calibur/sakura'
+
+export default {
+  name: 'EditTagRuleForm',
+  layout: 'app',
+  components: {
+    VForm,
+    VField,
+    VRange
+  },
+  data() {
+    return {
+      rule: null,
+      loading: false
+    }
+  },
+  computed: {
+    slug() {
+      return this.$route.query.slug
+    }
+  },
+  mounted() {
+    this.getRule()
+  },
+  methods: {
+    handleSubmit() {
+      if (this.loading) {
+        return
+      }
+      this.loading = true
+      this.$axios
+        .$post('v1/join/rule/update', {
+          ...this.rule,
+          bangumi_slug: this.slug
+        })
+        .then(() => {
+          this.$toast.success('更新成功')
+        })
+        .catch((err) => {
+          this.$toast.error(err.message)
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    getRule() {
+      this.$axios
+        .$get('v1/join/rule/show', {
+          params: {
+            slug: this.slug
+          }
+        })
+        .then((data) => {
+          this.rule = data
+        })
+        .catch((err) => {
+          this.$toast.error(err.message)
+        })
+    }
+  },
+  head() {
+    return {
+      title: '入圈规则'
+    }
+  }
+}
+</script>
